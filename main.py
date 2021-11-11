191cef0c06256fde1d62683b81751a13f71495fc
IlyaG96 <ilyag_96@icloud.com>
2021-11-12 06:43:11 +1100

    download photos from NASA

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import pathlib
 from urllib.parse import urlparse
 from datetime import datetime
 import os
+from dotenv import load_dotenv
 
 
 def download_image(url: str,
-                   path: str,
+                   PHOTO_PATH: str,
                    filename):
-    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(PHOTO_PATH).mkdir(parents=True, exist_ok=True)
 
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(f"{path}/{filename}", mode="wb") as file:
+    with open(f"{PHOTO_PATH}/{filename}", mode="wb") as file:
         file.write(response.content)
 
 
@@ -28,11 +29,11 @@ def get_info_spacex():
 def fetch_spacex_launch(image_links):
     for url_number, url in enumerate(image_links):
         filename = f"{url_number}.jpg"
-        download_image(url, path, filename)
+        download_image(url, PHOTO_PATH, filename)
 
 
-def get_NASA_apod(NASA_token):
-    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_token}"
+def get_NASA_apod(NASA_TOKEN):
+    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_TOKEN}"
     payload = {
         "count": "50",
         "thumbs": "True"
@@ -42,15 +43,15 @@ def get_NASA_apod(NASA_token):
     return urls
 
 
-def get_nasa_epic(NASA_token):
+def get_nasa_epic(NASA_TOKEN):
     urls =[]
-    address = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={NASA_token}"
+    address = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={NASA_TOKEN}"
     response = requests.get(address)
 
     for picture in response.json():
         name = picture['image']
         date = datetime.fromisoformat(picture['date']).date().strftime('%Y/%m/%d')
-        url = f"https://api.nasa.gov/EPIC/archive/natural/{date}/png/{name}.png?api_key={NASA_token}"
+        url = f"https://api.nasa.gov/EPIC/archive/natural/{date}/png/{name}.png?api_key={NASA_TOKEN}"
         urls.append(url)
     return urls
 
@@ -66,13 +67,23 @@ def download_pictures(urls):
     for number, url in enumerate(urls):
         extension = show_extension(url)
         filename = f"nasa{number}{extension}"
-        download_image(url, path, filename)
+        try:
+            download_image(url, PHOTO_PATH, filename)
+        except Exception as e:
+            print(e)
+            continue
 
 
 if __name__ == '__main__':
-    pass
-#  path
-#  token
+    load_dotenv()
+    NASA_TOKEN = os.getenv("NASA_TOKEN")
+    PHOTO_PATH = os.getenv("PHOTO_PATH")
+    urls = get_NASA_apod(NASA_TOKEN)
+    print(urls)
+    download_pictures(urls)
+
+
+
 
 
 
