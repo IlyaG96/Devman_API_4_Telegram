21aa6ced21202cc56d6a4b2c75af7b038196b95a
IlyaG96 <ilyag_96@icloud.com>
2021-11-12 18:48:28 +1100

    add custom delay to send_picture_tg

--- astrolivebot_script.py
+++ astrolivebot_script.py
@@ -1,11 +1,18 @@
+import time
+
 from dotenv import load_dotenv
 from os import listdir
 import os
 import telegram
 
 
-def send_text_message_tg(bot):
-    bot.send_message(chat_id=CHAT_ID, text="here is my first message")
+def send_text_message_tg(bot: telegram.Bot) -> None:
+    """
+    future function to send photo's descriptions
+    :param bot: telegram.Bot object
+    :return: None
+    """
+    bot.send_message(chat_id=CHAT_ID, text="Your text message")
 
 
 def generate_pictures_base(PHOTO_PATH: str) -> list:
@@ -18,9 +25,21 @@ def generate_pictures_base(PHOTO_PATH: str) -> list:
     return pictures_base
 
 
-def send_picture_tg(PHOTO_PATH, bot, files_base, ):
-    for file in files_base:
-        bot.send_photo(chat_id=CHAT_ID, photo=open(f"{PHOTO_PATH}{file}", 'rb'))
+def send_picture_tg(PHOTO_PATH: str,
+                    bot: telegram.Bot,
+                    files_base: list,
+                    delay = 86400):
+    """
+
+    :param PHOTO_PATH:
+    :param bot:
+    :param files_base:
+    :return:
+    """
+    while True:
+        for file in files_base:
+            bot.send_photo(chat_id=CHAT_ID, photo=open(f"{PHOTO_PATH}{file}", 'rb'))
+            time.sleep(delay)
 
 
 if __name__ == '__main__':
@@ -28,6 +47,7 @@ if __name__ == '__main__':
     TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
     PHOTO_PATH = os.getenv("PHOTO_PATH")
     CHAT_ID = os.getenv("CHAT_ID")
+    DELAY_CUSTOM = int(os.getenv("DELAY_CUSTOM"))
     bot = telegram.Bot(token=TG_TOKEN)
     files_base = generate_pictures_base(PHOTO_PATH)
-    send_picture_tg(PHOTO_PATH, bot, files_base)
+    send_picture_tg(PHOTO_PATH, bot, files_base, delay=DELAY_CUSTOM)
